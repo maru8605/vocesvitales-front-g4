bf1cc7bfacab21da93ea2e8894b41b25505a485d
FlorApreda <florencia.apreda@gmail.com>
2020-12-13 15:29:28 -0300

    arregle img y estilos

--- src/App.js
+++ src/App.js
@@ -1,32 +1,24 @@
-import React from 'react';
-import Header from '../src/sections/Header/Header'
-import './App.css';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Carrusel from './components/Carrusel'
-import Modal from './components/Modal';
-import Form from './components/Form'
-import './form.css';
-import About from './sections/About/About'
-import Subscribe from './sections/Subscribe/Subscribe'
-import Partnership from './sections/Partnership/Partnership'
+import React from 'react'
 
+import './App.css'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 
+import Modal from './components/Modal'
+import Form from './components/Form/Form'
+import Home from './pages/Home'
 
 function App() {
-return <>
-  <Router>
-    <Header/>
-    <Carrusel/>
-    <About />
-		<Subscribe />
-		<Partnership />
-    <Switch>
-      <Route path='/form' component={Form}/> 
-      <Modal/>
-    </Switch>           
-  </Router>
-</>
+	return (
+		<>
+			<Router>
+				<Switch>
+					<Route path="/form" component={Form} />
+					<Route path="/" component={Home} />
+					<Modal />
+				</Switch>
+			</Router>
+		</>
+	)
 }
 
-
-export default App;
+export default App
---
+++ src/sections/Programas/Programas.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import './Programas.css'
+
+function Programas() {
+	return <section></section>
+}
+
+export default Programas
